28267604d2dcb4c780061fe1d79c8871ec3551dd
reineke <elisa.reineke@student.fh-kiel.de>
2024-03-04 13:16:20 +0100

    drop down with valid standort Ids in person-add-form

--- src/app/view/person-view/person-add/person-add-form.component.ts
+++ src/app/view/person-view/person-add/person-add-form.component.ts
@@ -1,9 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
 import {Router, RouterModule} from "@angular/router";
 import {Person} from "../../../model/person";
 import {PersonService} from "../../../service/person-service.service";
+import {StandortService} from "../../../service/standort-service.service";
 
 
 @Component({
@@ -13,14 +14,23 @@ import {PersonService} from "../../../service/person-service.service";
   templateUrl: './person-add-form.component.html',
   styleUrl: './person-add-form.component.css'
 })
-export class PersonAddFormComponent {
-
+export class PersonAddFormComponent implements OnInit {
+  standortIds: string[] = [];
   person: Person;
 
-  constructor(
-    private router: Router,
-    private personService: PersonService) {
+  constructor(private standortService: StandortService,
+              private router: Router,
+              private personService: PersonService) {
     this.person = new Person();
+
+  }
+
+  ngOnInit(): void {
+    this.standortService.findAll().subscribe(standorte => {
+      standorte.forEach(standort => {
+        this.standortIds.push(standort.id);
+      })
+    });
   }
 
   onSubmit() {
@@ -31,4 +41,6 @@ export class PersonAddFormComponent {
   gotoPersonIndex() {
     this.router.navigate(['/person/index']);
   }
+
+
 }
